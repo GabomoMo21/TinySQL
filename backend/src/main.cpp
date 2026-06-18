dbedcb6d8e71cf6f51b53457890bcbf8db6759a3
GabomoMo21 <g.morales.1@estudiantec.cr>
2026-06-18 13:57:31 -0600

    crear proyecto base del backend

---
+++ backend/src/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main()
+{
+    // Este mensaje permite comprobar que el backend se compiló y arrancó correctamente.
+    std::cout << "TinySQLDb backend iniciado correctamente.\n";
+
+    return 0;
+}
